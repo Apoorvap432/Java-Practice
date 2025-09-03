defc0ba01dc4be9c82a7b66fcd89c32ef3c8267f
apoorva-revature <apoorvapanwar4@gmail.com>
2025-09-03 16:58:12 +0530

    3. Variables

--- Method.java
+++ Method.java
@@ -6,7 +6,6 @@ public class Method {
     public static void main(String[] args) {
         int sumOfNumbers = sum(3, 5);
         System.out.println(sumOfNumbers);
-
     }
 
 }
---
+++ Variable.java
@@ -0,0 +1,12 @@
+//there are 8 variables - Bitter Black Coffee Sure Is For Long Days - Boolean, byte, char, short, Int, Float, Long, Double
+
+public class Variable {
+    boolean b = true;                   //true or false
+    Byte a = 120;                       //value from -127 to 127
+    char c = 'C';                       //contains alphabets
+    short s = 32767;                    // contains -32768 to 32767
+    int i = 2147483647;                 // contains -2,147,483,648 to 2,147,483,647 
+    float f = 1.234F;                   // contains values in decimal forms - less accurate
+    long l = 9223372036854775807L;      // very large range 
+    double d = 87.54848;                //decimal - more accurate
+}
