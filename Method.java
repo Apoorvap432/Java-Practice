c9b7e4427f8adf37478285cc457b93f722665438
apoorva-revature <apoorvapanwar4@gmail.com>
2025-09-03 18:50:14 +0530

    2. Method

--- Method.java
+++ Method.java
@@ -2,7 +2,7 @@ public class Method {
     static int sum(int a, int b) {
         return a+b;
     }
-
+    //below is the main method islye hm iske andr dusra method ni likh skte islye sum method iske bhr likha 
     public static void main(String[] args) {
         int sumOfNumbers = sum(3, 5);
         System.out.println(sumOfNumbers);
