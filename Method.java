920f39a9a9907d678e49596006c660f7c7461a51
apoorva-revature <apoorvapanwar4@gmail.com>
2025-09-03 16:48:05 +0530

    2. Methods

---
+++ Method.java
@@ -0,0 +1,12 @@
+public class Method {
+    static int sum(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        int sumOfNumbers = sum(3, 5);
+        System.out.println(sumOfNumbers);
+
+    }
+
+}
