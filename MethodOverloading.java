3147b0d1d3ac8360ac03569d39aeb9a07829a8c0
Apoorvap432 <apoorvapanwar488@gmail.com>
2025-09-07 18:47:48 +0530

    16A. Polymorphism-Method overloading

---
+++ MethodOverloading.java
@@ -0,0 +1,27 @@
+//ek hi action (method) alag-alag tariko se perform ho sakta hai.
+
+
+//Compile time/static polymorphism or Method overloading
+
+class DisplayOverloading{
+    void disp(char c){
+        System.out.println(c);
+    }
+    void disp(int a, char b){
+        System.out.println(a + " " + b);
+    }
+    void disp(int t, char y, int a){
+        System.out.println(t + " " + y + " " + a);
+    }
+}
+public class Polymorphism {
+    public static void main(String[] args) {
+        DisplayOverloading obj = new DisplayOverloading();
+        obj.disp('a');
+        obj.disp(7, 'g');
+        obj.disp(4, 'z', 0);
+
+
+    }
+    
+}
