52d56c8538595f706ebcf25dc67b637e4e3b5a7e
Apoorvap432 <apoorvapanwar488@gmail.com>
2025-10-31 18:31:46 +0530

    dependency injection

---
+++ DependencyInjection.java
@@ -0,0 +1,26 @@
+//without dependency injection
+
+// public class DependencyInjection {
+//     private BookService bookService;
+
+//     public DependencyInjection(){
+//         this.bookService = new bookService();
+//     }
+
+//     public list<Book> getBooks(){
+//         return bookService.getBooks();
+//     }
+// }
+
+
+
+
+//with dependency injection
+
+// public class BookStore {
+//     private BookService bookService;
+
+//     public BookStore(BookService bookService){
+//         this.bookService = bookService;
+//     }
+// }
--- Handlers.java
+++ Handlers.java
@@ -1,8 +1,10 @@
 //step 1- model (basic class)
-public class Handlers {
-    public final int id;
-    public final String name;
-}
+// public class Handlers {
+//     public final int id;
+//     public final String name;
+// }
+
+
 
 //step-2 Data Access Object
 // class UserDao {
@@ -13,7 +15,6 @@ public class Handlers {
 //     );
 
 //     private static UserDao userDao = null;
-
 //     static UserDao instance() {
 //         if (userDao == null) {
 //             userDao = new UserDao();
@@ -25,3 +26,30 @@ public class Handlers {
 //     Iterable<String> getAllUsernames() { ... }
 // }
 
+
+
+// Step 3: Controller (UserController.java)
+// public class UserController {
+//     public static Handler fetchAllUsernames = ctx -> {
+//         UserDao dao = UserDao.instance();
+//         Iterable<String> allUsers = dao.getAllUsernames();
+//         ctx.json(allUsers);
+//     };
+
+//     public static Handler fetchById = ctx -> {
+//         int id = Integer.parseInt(ctx.pathParam("id"));
+//         UserDao dao = UserDao.instance();
+//         Optional<User> user = dao.getUserById(id);
+
+//         if (user.isPresent()) {
+//             ctx.json(user.get());
+//         } else {
+//             ctx.html("Not Found");
+//         }
+//     };
+// }
+
+
+// Step 4: Routes Register Karna
+// app.get("/users", UserController.fetchAllUsernames);
+// app.get("/users/{id}", UserController.fetchById);
