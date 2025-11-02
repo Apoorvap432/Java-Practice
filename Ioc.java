0c53710e6e5b362cf2674057fd0d4b58c50f16a1
Apoorvap432 <apoorvapanwar488@gmail.com>
2025-11-02 15:35:04 +0530

    Inversion of control

---
+++ Ioc.java
@@ -0,0 +1,23 @@
+//ioc example
+
+public interface Ioc {
+    void drive();
+}
+
+public class Car implements Ioc{
+    public void drive(){
+        System.out.println("Car will drive");
+    }
+}
+
+public class Bike implements Ioc{
+    public void drive(){
+        System.out.println("Bike will drive");
+    }
+}
+
+<bean id="vehicle" class="Revature.Bike"></bean>
+
+ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
+Ioc v = (Ioc) context.getBean("Ioc");
+v.drive();
