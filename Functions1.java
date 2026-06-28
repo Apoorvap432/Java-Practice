2f39030eee1ef2f44bb3c3ca746ed9d7c0789108
Apoorvap432 <apoorvapanwar488@gmail.com>
2026-06-28 17:03:17 +0530

    functions 1 and functions 2 done with multiplication and addition function

--- Functions1.java
+++ Functions1.java
@@ -1,11 +1,10 @@
 import java.util.*;
 
-public class Functions{
+public class Functions1{
     public static int CalculateSum(int a, int b){
         int sum = a+b;
         return sum;
     }
-
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
