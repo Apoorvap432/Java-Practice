7f3b3f8cef58bf887fcb365ac804cb0cc4a0253b
Apoorvap432 <apoorvapanwar488@gmail.com>
2026-06-30 22:33:09 +0530

    code till functions

--- Functions2.java
+++ Functions2.java
@@ -1,5 +1,18 @@
 import java.util.*;
 
 public class Functions2 {
-    
+    public static int Multiply(int a, int b){
+        int multiply = a * b;
+        return multiply;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int multiply = Multiply(a, b);
+        System.out.println("Product is: " + multiply);
+    }
+
 }
